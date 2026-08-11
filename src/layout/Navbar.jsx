4eda1bdaa5e105f8afee67e74d3b9bc88b7456ab
samirabelmabkhoute@gmail.com <samirabelmabkhoute@gmail.com>
2026-08-12 00:53:02 +0200

    update

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
           href="#"
           className="text-xl font-bold tracking-tight hover:text-primary"
         >
-          PM<span className="text-primary">.</span>
+          SB<span className="text-primary">.</span>
         </a>
 
         {/* Desktop Nav */}
